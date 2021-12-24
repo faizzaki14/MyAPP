e13c7e895fba2729c25badc7dee6f85cca67afa0
faizzaki14 <19104075@ittelkom-pwt.ac.id>
2021-12-24 18:13:09 +0700

    Home Init

---
+++ Home/app/src/main/java/com/faizzakiramadhan_19104075/myapplication/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.faizzakiramadhan_19104075.myapplication.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
