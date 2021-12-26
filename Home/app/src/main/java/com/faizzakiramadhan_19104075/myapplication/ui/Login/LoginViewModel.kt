cd8790c9b43990d85549b412827442aec098e7cb
anggeralmasih <angger.almasih@gmail.com>
2021-12-26 19:53:48 +0700

    tambah frg login, reg, drwbl

---
+++ Home/app/src/main/java/com/faizzakiramadhan_19104075/myapplication/ui/Login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.faizzakiramadhan_19104075.myapplication.ui.Login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ Home/app/src/main/java/com/faizzakiramadhan_19104075/myapplication/ui/Payment/PaymentViewModel.kt
@@ -0,0 +1,7 @@
+package com.faizzakiramadhan_19104075.myapplication.ui.Payment
+
+import androidx.lifecycle.ViewModel
+
+class PaymentViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
