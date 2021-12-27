2cb36b37e72571bb70564598541dccd28dedc1ff
faizzaki14 <19104075@ittelkom-pwt.ac.id>
2021-12-27 13:56:02 +0700

    add Map

--- Home/app/src/main/java/com/faizzakiramadhan_19104075/myapplication/MainActivity.kt
+++ Home/app/src/main/java/com/faizzakiramadhan_19104075/myapplication/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.faizzakiramadhan_19104075.myapplication
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.animation.Animation
 import android.widget.Button
+import androidx.appcompat.app.ActionBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.faizzakiramadhan_19104075.myapplication.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.SupportMapFragment
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
@@ -34,8 +40,12 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications,R.id.navigation_profile
             )
         )
+
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
     }
 
 }
