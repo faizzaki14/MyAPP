f8f0fc6c02e748ac9c9ddc80ca44668a90177e63
anggeralmasih <angger.almasih@gmail.com>
2022-01-13 10:40:27 +0700

    Delete Broadcast di Splashscreen

--- Home/app/src/main/java/com/faizzakiramadhan_19104075/myapplication/SplashScreenActivity.kt
+++ Home/app/src/main/java/com/faizzakiramadhan_19104075/myapplication/SplashScreenActivity.kt
@@ -6,12 +6,9 @@ import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.faizzakiramadhan_19104075.myapplication.ui.BroadCastReceiver.InternetBroadcastReceiver
 import com.faizzakiramadhan_19104075.myapplication.ui.Login.LoginActivity
 
 class SplashScreenActivity : AppCompatActivity() {
-
-    lateinit var receiver: InternetBroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,16 +21,6 @@ class SplashScreenActivity : AppCompatActivity() {
             finish()
         }, 4000)
 
-        // internet receiver
-        receiver = InternetBroadcastReceiver()
-        IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION).also {
-            registerReceiver(receiver, it)
-        }
-
     }
 
-    override fun onStop() {
-        super.onStop()
-        unregisterReceiver(receiver)
-    }
 }
