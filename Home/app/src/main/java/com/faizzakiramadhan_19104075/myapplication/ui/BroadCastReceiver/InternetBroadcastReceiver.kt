925c8eca331fc0564f0425273a80aa9a8bff7424
anggeralmasih <angger.almasih@gmail.com>
2022-01-13 10:27:32 +0700

    BroadcastReceiver

---
+++ Home/app/src/main/java/com/faizzakiramadhan_19104075/myapplication/ui/BroadCastReceiver/InternetBroadcastReceiver.kt
@@ -0,0 +1,22 @@
+package com.faizzakiramadhan_19104075.myapplication.ui.BroadCastReceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.widget.Toast
+
+class InternetBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (ConnectivityManager.CONNECTIVITY_ACTION == intent.action) {
+            val noConnectivity = intent.getBooleanExtra(
+                ConnectivityManager.EXTRA_NO_CONNECTIVITY, false
+            )
+            if (noConnectivity) {
+                Toast.makeText(context, "Disconnected", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "Connected", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
